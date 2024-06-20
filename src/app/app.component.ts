f9b2c17fb7e1be1f2d88437697a691361be1e5dc
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-06-20 10:51:29 +0300

    Реализован отступ body в зависимости от размера навбара

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, ElementRef, Inject, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {NavBarComponent} from "./nav-bar/nav-bar.component";
+import {HomeComponent} from "./home/home.component";
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(
+    private element: ElementRef,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+  }
+
+  navHeight: number = 0
   title = 'django-project-frontend';
+
+  ngOnInit() {
+    this.navHeight = this.element.nativeElement.querySelector('.navbar').offsetHeight
+    this.updateBodyPadding(this.navHeight)
+  }
+
+  updateBodyPadding(padding: number = 0): void {
+    this.document.body.style.paddingTop = `${padding}px`
+  }
 }
