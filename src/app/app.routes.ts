8ef45ac47c602755b313b3b7629f3d8a663f6b11
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-06-24 16:54:22 +0300

    Добавлен редирект на домашнюю страницу

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
     path: 'home',
     loadChildren: () => import('./home/home.routes').then(m => m.routes),
     title: 'Home page'
