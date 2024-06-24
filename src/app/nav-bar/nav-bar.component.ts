35aa9f14481e0aa5cd58b5bfb1dbb46999d6775b
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-06-24 16:51:30 +0300

    Обновлены импорты

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -2,11 +2,12 @@ import {Component, ViewChild} from '@angular/core';
 
 import {NgbDatepickerModule} from '@ng-bootstrap/ng-bootstrap';
 import {NavOffcanvasComponent} from "./nav-offcanvas/nav-offcanvas.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'my-nav-bar',
   standalone: true,
-  imports: [NgbDatepickerModule, NavOffcanvasComponent],
+  imports: [NgbDatepickerModule, NavOffcanvasComponent, RouterLink],
   templateUrl: './nav-bar.component.html',
 })
 export class NavBarComponent {
