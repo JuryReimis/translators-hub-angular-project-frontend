7b7102bc8bc3a85d12d418e31dc3e81f1e9ee758
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-06-24 16:30:08 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.routes').then(m => m.routes),
+    title: 'Home page'
+  },
+  {
+    path: 'projects',
+    loadChildren: () => import('./projects/projects.routes').then(m => m.routes),
+    title: "Projects page"
+  },
+];
