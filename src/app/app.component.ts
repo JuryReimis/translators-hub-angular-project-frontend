386af6001893d610bdb97404b13b6d44d3bedad2
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-06-24 16:55:28 +0300

    Добавлен RouterLink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, ElementRef, Inject, OnInit} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {NavBarComponent} from "./nav-bar/nav-bar.component";
 import {HomeComponent} from "./home/home.component";
 import {DOCUMENT} from "@angular/common";
@@ -9,7 +9,7 @@ import {ProfilePageComponent} from "./profile-page/profile-page.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, HomeComponent, ProjectsComponent, ProfilePageComponent],
+  imports: [RouterOutlet, NavBarComponent, HomeComponent, ProjectsComponent, ProfilePageComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
