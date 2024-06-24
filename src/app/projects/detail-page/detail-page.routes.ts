8e05dd224fb77fd58071c063bd8ff1eb024f69f6
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-06-24 16:30:52 +0300

    Создана маршрутизация для компонента

---
+++ src/app/projects/detail-page/detail-page.routes.ts
@@ -0,0 +1,11 @@
+import {Routes} from "@angular/router";
+import {DetailPageComponent} from "./detail-page.component";
+
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: DetailPageComponent,
+    title: "Detail page"
+  }
+]
---
+++ src/app/projects/projects.routes.ts
@@ -0,0 +1,16 @@
+import {Routes} from "@angular/router";
+import {ProjectsComponent} from "./projects.component";
+
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: ProjectsComponent,
+    title: "Projects page"
+  },
+  {
+    path: ':slug',
+    loadChildren: () => import('./detail-page/detail-page.routes').then(m => m.routes),
+    title: "Detail page"
+  }
+]
