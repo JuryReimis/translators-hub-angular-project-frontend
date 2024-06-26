2e751b571012ca936123c09c5e78fc39df652427
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-06-26 17:18:25 +0300

    Создан файл маршрутизации для компонента управления проектом

---
+++ src/app/projects/detail-page/project-management/project-management.routes.ts
@@ -0,0 +1,11 @@
+import {Routes} from "@angular/router";
+import {ProjectManagementComponent} from "./project-management.component";
+
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: ProjectManagementComponent,
+    title: "Project edit page"
+  }
+]
