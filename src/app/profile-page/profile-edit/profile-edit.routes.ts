ff653df0e98178cfba91e003ef1d96c4b2cc2dae
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-06-26 18:56:27 +0300

    Создан файл маршрутизации для страницы изменения профиля

---
+++ src/app/profile-page/profile-edit/profile-edit.routes.ts
@@ -0,0 +1,11 @@
+import {Routes} from "@angular/router";
+import {ProfileEditComponent} from "./profile-edit.component";
+
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: ProfileEditComponent,
+    title: 'Edit profile page'
+  }
+]
