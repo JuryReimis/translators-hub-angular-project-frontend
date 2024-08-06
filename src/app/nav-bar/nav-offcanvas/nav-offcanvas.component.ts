a09ab2c504cd2cf8d267d4c7b36d2a0cc1180c85
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-08-06 15:33:45 +0300

    Добавлено изменение надписи в родительском шаблоне в зависимости от наличия токена авторизации

--- src/app/nav-bar/nav-offcanvas/nav-offcanvas.component.ts
+++ src/app/nav-bar/nav-offcanvas/nav-offcanvas.component.ts
@@ -1,8 +1,11 @@
-import {Component, inject, Input, Output, TemplateRef, ViewChild} from '@angular/core';
+import {Component, inject, OnInit, Output, TemplateRef, ViewChild} from '@angular/core';
 
 import {NgbDatepickerModule, NgbOffcanvas, OffcanvasDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 import {NgIf} from "@angular/common";
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
+import {UserProfileService} from "../../services/user-profile.service";
+import {AuthService} from "../../services/auth.service";
+import {IUser, IUserProfile} from "../../models/authentication";
 
 @Component({
   selector: 'nav-offcanvas',
@@ -10,14 +13,17 @@ import {RouterLink} from "@angular/router";
   imports: [NgbDatepickerModule, NgIf, RouterLink],
   templateUrl: 'nav-offcanvas.component.html',
 })
-export class NavOffcanvasComponent {
+export class NavOffcanvasComponent implements OnInit{
   private offcanvasService = inject(NgbOffcanvas);
   closeResult = '';
 
-  user_logged: string = 'Hey'
-  @Output() authentication_status: string = 'Логин'
+  @Output() authenticationStatus: string = ''
 
-  @Input() user: string = '';
+  private authToken: string
+  userLogged: boolean = false
+  private user: IUser
+  private userProfile: IUserProfile
+  private router: Router
 
   @ViewChild('content') content!: TemplateRef<any>
 
@@ -30,6 +36,28 @@ export class NavOffcanvasComponent {
         this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
       },
     );
+  }
+
+  constructor(private userProfileService: UserProfileService, private authService: AuthService) {
+  }
+
+  ngOnInit() {
+    this.authToken = this.authService.getToken()
+    if (this.authToken) {
+      this.userProfileService.getUserByToken(this.authToken).subscribe(userData => {
+        this.user = userData.user
+        this.userProfile = userData.userProfile
+        this.authenticationStatus = this.user.username
+        this.userLogged = true
+      })
+    }
+    else {
+      this.authenticationStatus = "Login/Registration"
+    }
+  }
+
+  openLoginModal() {
+    this.authService.openLoginWindow()
   }
 
   private getDismissReason(reason: any): string {
