06470ab9814fc6fedd5142c8d1df9d5b4ee58ec0
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-09-12 21:39:55 +0300

    Добавлен поток для отслеживания авторизованного пользователя. Изменены методы logIn и logOut Добавлены методы для получения авторизованного пользователя и сброса при выходе из системы

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,11 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {catchError, Observable, Subscription, tap, throwError} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {BehaviorSubject, catchError, Observable, Subscription, tap, throwError} from "rxjs";
 import {CookieService} from "ngx-cookie-service";
 import {NgbModal, NgbModalRef} from "@ng-bootstrap/ng-bootstrap";
 import {LoginComponent} from "../auth/login/login.component";
+import {IToken, IUser} from "../models/authentication";
+import {UserService} from "./user.service";
 
 @Injectable({
   providedIn: 'root',
@@ -11,43 +13,76 @@ import {LoginComponent} from "../auth/login/login.component";
 export class AuthService {
 
   private authToken: string = ''
+  private loggedUser$: BehaviorSubject<IUser | null> = new BehaviorSubject<IUser | null>(null)
   private modalRef: NgbModalRef
 
 
-  constructor(private httpService: HttpClient, private cookieService: CookieService, private modalService: NgbModal) {
+  constructor(private httpService: HttpClient, private cookieService: CookieService, private modalService: NgbModal,
+              private userService: UserService) {
+    this.checkAuth()
   }
 
   openLoginWindow() {
-    this.modalRef =  this.modalService.open(LoginComponent)
+    this.modalRef = this.modalService.open(LoginComponent)
   }
 
   closeModal(result?: string) {
     this.modalRef.close(result)
   }
 
-  logIn(userName: string, password: string): Observable<string> {
-    return this.httpService.post<string>('http://127.0.0.1:8000/auth/token/login', {
+  logIn(userName: string, password: string): Observable<IToken> {
+    return this.httpService.post<IToken>('http://127.0.0.1:8000/auth/token/login', {
       'username': userName,
       'password': password
     }).pipe(
       tap(
         (authToken) => {
-          this.setToken(authToken)
+          this.setToken(authToken.auth_token)
+          this.getLoggedUser()
         }),
       catchError(
         (error: any) => {
-          console.error(error)
+          console.error('Login error', error)
           return throwError(() => new Error(error))
         }))
   }
 
-  logOut(): Subscription {
-    return this.httpService.post<string>('http://127.0.0.1:8000/auth/token/logout', {}).subscribe(
-      (status: string) => {
-        this.destroyToken()
-      }
-    )
+  logOut() {
+    console.log('logout')
+    const headers: HttpHeaders = new HttpHeaders({
+      'Authorization': `Token ${this.authToken}`
+    })
+    return this.httpService.post<any>('http://127.0.0.1:8000/auth/token/logout', {}, {headers: headers}).pipe(tap(
+        (status: string) => {
+          this.destroyToken()
+          this.resetLoggedUser()
+          this.checkAuth()
+        }
+      ),
+      catchError(
+        (error: any) => {
+          console.error('Logout error', error)
+          return throwError(() => new Error(error))
+        }
+      ))
   }
+
+  resetLoggedUser() {
+    this.loggedUser$.next(null)
+  }
+
+  getLoggedUser() {
+    if (!this.loggedUser$.value) {
+      const userByToken = this.userService.getUserByToken(this.authToken)
+      if (userByToken) {
+        userByToken.subscribe((user) => {
+          this.loggedUser$.next(user)
+        })
+      }
+    }
+    return this.loggedUser$
+  }
+
 
   getToken() {
     return this.authToken
@@ -56,6 +91,7 @@ export class AuthService {
   checkAuth() {
     this.authToken = this.cookieService.get('auth_token')
     if (this.authToken) {
+      this.getLoggedUser()
       return this.authToken
     } else {
       return false
