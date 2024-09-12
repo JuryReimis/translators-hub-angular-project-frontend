38e2927d0c06714ea161bab4a19c7c8206dab3fd
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-09-12 21:51:16 +0300

    Добавлено получение данных об авторизированном пользователе через поток. Вывод динамического заголовка кнопки

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -4,15 +4,38 @@ import {NgbDatepickerModule} from '@ng-bootstrap/ng-bootstrap';
 import {NavOffcanvasComponent} from "./nav-offcanvas/nav-offcanvas.component";
 import {RouterLink} from "@angular/router";
 import {LoginComponent} from "../auth/login/login.component";
+import {AuthService} from "../services/auth.service";
+import {IUser} from "../models/authentication";
+import {BehaviorSubject, Observable, map} from "rxjs";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'my-nav-bar',
   standalone: true,
-  imports: [NgbDatepickerModule, NavOffcanvasComponent, RouterLink, LoginComponent],
+  imports: [NgbDatepickerModule, NavOffcanvasComponent, RouterLink, LoginComponent, AsyncPipe],
   templateUrl: './nav-bar.component.html',
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit{
   @ViewChild(NavOffcanvasComponent) private navOffcanvasComponent!: NavOffcanvasComponent;
+
+  private loggedUser$: BehaviorSubject<IUser | null>
+  public status$: Observable<IUser|string>
+
+  ngOnInit() {
+    this.loggedUser$ = this.authService.getLoggedUser()
+    this.status$ = this.loggedUser$.asObservable().pipe(
+      map((user) => {
+        if (user) {
+          return user.username;
+        } else {
+          return 'Login/Register';
+        }
+      })
+    );
+  }
+
+  constructor(private authService: AuthService) {
+  }
 
   openOffcanvas() {
     this.navOffcanvasComponent.open(this.navOffcanvasComponent.content)
