a52362faf903701ea3ddfe6a2a055074d6e9bda3
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-10-09 17:33:17 +0300

    Create storage.service.ts Функции хранения и обработки токена авторизации перенесены в новый сервис

---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import {CookieService} from "ngx-cookie-service";
+import {BehaviorSubject} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService{
+
+  private authToken: string
+  private authToken$: BehaviorSubject<string|null> = new BehaviorSubject<string | null>(null)
+
+  constructor(private cookieService: CookieService) {
+    if (cookieService.check('auth_token')) {
+      console.log('имеется', this.cookieService.get('auth_token'))
+      this.authToken$.next(cookieService.get('auth_token'))
+      this.authToken = cookieService.get('auth_token')
+    }
+    else {
+      console.log('Не имеется')
+    }
+    this.authToken = this.cookieService.get('auth_token')
+    console.log('constructor', this.authToken)
+  }
+
+  getToken() {
+    console.log('getToken')
+    return this.authToken
+  }
+
+  getToken$() {
+    return this.authToken$.asObservable()
+  }
+
+  setToken(authToken: string) {
+    this.cookieService.set('auth_token', authToken, 2, '/', undefined, false, 'Strict')
+    this.authToken = authToken
+  }
+
+  destroyToken() {
+    this.cookieService.delete('auth_token')
+    this.authToken = ''
+  }
+}
