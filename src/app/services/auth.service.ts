cc44aa94d6b03817033851d7c4c0335672fdef3e
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-10-14 13:18:53 +0300

    Улучшены методы сервиса Оптимизирован метод checkAuth В метод logIn добавлен контекст, сигнализирующий о том, что не нужно отправлять токен авторизации с запросом.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,23 +1,26 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient, HttpContext} from "@angular/common/http";
 import {BehaviorSubject, catchError, Observable, tap, throwError} from "rxjs";
-import {CookieService} from "ngx-cookie-service";
 import {NgbModal, NgbModalRef} from "@ng-bootstrap/ng-bootstrap";
 import {LoginComponent} from "../auth/login/login.component";
 import {IToken, IUser} from "../models/authentication";
 import {UserService} from "./user.service";
+import {StorageService} from "./storage.service";
+import {NEED_AUTH} from "../constants/auth.context-token";
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
 
-  private authToken: string = ''
+  private authToken$: Observable<string | null>
   private loggedUser$: BehaviorSubject<IUser | null> = new BehaviorSubject<IUser | null>(null)
+  private isFirstServiceInit$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true)
+
   private modalRef: NgbModalRef
 
 
-  constructor(private httpService: HttpClient, private cookieService: CookieService, private modalService: NgbModal,
+  constructor(private httpService: HttpClient, private storageService: StorageService, private modalService: NgbModal,
               private userService: UserService) {
     this.checkAuth()
   }
@@ -30,33 +33,30 @@ export class AuthService {
     this.modalRef.close(result)
   }
 
-  logIn(userName: string, password: string): Observable<IToken> {
+  logIn(userName: string, password: string) {
     return this.httpService.post<IToken>('http://127.0.0.1:8000/auth/token/login', {
-      'username': userName,
-      'password': password
-    }).pipe(
+        'username': userName,
+        'password': password
+      },
+      {context: new HttpContext().set(NEED_AUTH, false),}).pipe(
       tap(
         (authToken) => {
-          this.setToken(authToken.auth_token)
-          this.checkAuth()
+          this.storageService.setToken(authToken.auth_token)
+          return authToken
         }),
       catchError(
         (error: any) => {
           console.error('Login error', error)
-          return throwError(() => new Error(error))
+          throw error
         }))
   }
 
   logOut() {
     console.log('logout')
-    const headers: HttpHeaders = new HttpHeaders({
-      'Authorization': `Token ${this.authToken}`
-    })
-    return this.httpService.post<any>('http://127.0.0.1:8000/auth/token/logout', {}, {headers: headers}).pipe(tap(
+    return this.httpService.post<any>('http://127.0.0.1:8000/auth/token/logout', {}).pipe(tap(
         (status: string) => {
-          this.destroyToken()
+          this.storageService.destroyToken()
           this.resetLoggedUser()
-          this.checkAuth()
         }
       ),
       catchError(
@@ -69,51 +69,39 @@ export class AuthService {
 
   resetLoggedUser() {
     this.loggedUser$.next(null)
+    this.isFirstServiceInit$.next(true)
   }
 
   getLoggedUser() {
-    return this.loggedUser$
-  }
-
-
-  getToken() {
-    return this.authToken
+    // Возвращает поток с пользователем, который вошел в систему
+    return this.loggedUser$.asObservable()
   }
 
   checkAuth() {
-    // Проверка наличия авторизации. Возвращает True, если в сервисе уже сохранен авторизированный пользователь или если
-    // запрос по токену произошел успешно. False во всех остальных случаях
+    // Проверка наличия авторизации.
 
-    this.authToken = this.cookieService.get('auth_token')
-    if (this.authToken) {
-      // Если в куки есть токен авторизации
-      if (!this.loggedUser$.value) {
-        // Если в сервисе еще не сохранен авторизированный пользователь
-        const userByToken$ = this.userService.getUserByToken(this.authToken)
-        if (userByToken$) {
-          userByToken$.subscribe((user) => {
-            this.loggedUser$.next(user)
-          })
-          return true
-        } else {
-          this.loggedUser$.next(null)
-          return false
+    this.authToken$ = this.storageService.getToken$()
+
+
+    this.authToken$.subscribe((authToken: string | null) => {
+        if (authToken) {
+          // Если в куки есть токен авторизации
+          if (!this.loggedUser$.value) {
+            // Если в сервисе еще не сохранен авторизированный пользователь
+            if (this.isFirstServiceInit$.value) {
+              this.isFirstServiceInit$.next(false)
+              const userByToken$ = this.userService.getUserByToken()
+              if (userByToken$) {
+                userByToken$.subscribe((user) => {
+                  this.loggedUser$.next(user)
+                })
+              } else {
+                this.resetLoggedUser()
+              }
+            }
+          }
         }
       }
-      else {
-        return true
-      }
-    }
-    else {
-      return false
-    }
-  }
-
-  private setToken(authToken: string) {
-    this.cookieService.set('auth_token', authToken, 2, '/', undefined, false, 'Strict')
-  }
-
-  private destroyToken() {
-    this.cookieService.delete('auth_token')
+    )
   }
 }
