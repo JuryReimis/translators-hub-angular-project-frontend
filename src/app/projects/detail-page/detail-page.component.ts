45e72e1988c77bc9ff24cf53f61f119013c9bf0f
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-01-30 16:55:28 +0300

    Добавлен метод сортировки списка авторов по группам, исходя из их роли.

--- src/app/projects/detail-page/detail-page.component.ts
+++ src/app/projects/detail-page/detail-page.component.ts
@@ -1,10 +1,10 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {AsyncPipe, NgForOf, NgIf} from "@angular/common";
-import {IProject} from "../../models/projects";
+import {IProject, IRoles, ISortedRoles} from "../../models/projects";
 import {IUser} from "../../models/authentication";
 import {ActivatedRoute, RouterLink, RouterOutlet} from "@angular/router";
 import {ProjectsService} from "../../services/projects.service";
-import {Observable} from "rxjs";
+import {Observable, tap} from "rxjs";
 
 
 @Component({
@@ -20,9 +20,10 @@ import {Observable} from "rxjs";
 })
 export class DetailPageComponent implements OnInit{
   route: ActivatedRoute = inject(ActivatedRoute)
-  pk: string
+  pk: number
   moderator: boolean = true
-  pageData$: Observable<IProject>
+  pageData$: Observable<IProject|null>
+  sortedRoles: ISortedRoles
 
   constructor(private projectService: ProjectsService) {
   }
@@ -30,7 +31,40 @@ export class DetailPageComponent implements OnInit{
   protected readonly Object = Object;
 
   ngOnInit() {
-    this.pk = String(this.route.snapshot.params['pk'])
-    this.pageData$ = this.projectService.getProject(this.pk)
+    this.pk = Number(this.route.snapshot.params['pk'])
+    this.pageData$ = this.projectService.getProject(this.pk).pipe(tap(
+      (value: IProject|null) => {
+        if (value) {
+          this.sortedRoles = this.sortAuthorsByRoles(value.authors)
+        }
+      }
+    ))
+  }
+
+
+  sortAuthorsByRoles(authors: IRoles[]): ISortedRoles {
+
+    const sortedAuthors: ISortedRoles = new class implements ISortedRoles {
+      [key: string]: any;
+
+      mdr: IUser[];
+      org: IUser[];
+      trs: IUser[];
+      tst: IUser[];
+    };
+
+    authors.forEach((author: IRoles) => {
+      const role = author.role;
+      const user = author.user;
+
+      // Если роль еще не существует в sortedAuthors, добавляем ее
+      if (!sortedAuthors[role]) {
+        sortedAuthors[role] = [];
+      }
+
+      // Добавляем пользователя в соответствующую роль
+      sortedAuthors[role].push(user);
+    });
+    return sortedAuthors;
   }
 }
